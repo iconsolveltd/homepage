d89d88c30181620c89592fbdf959bc4c8cda70bb
iconsolveltd <paul.amissah@outlook.com>
2025-03-27 01:12:20 +0000

    Updated page title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Iconsolve - Tech Consultants & Trainers</title>
+    <title>Iconsolve - Your Reliable Tech Consultants</title>
     <meta name="description" content="Iconsolve - Tech Consultants & Trainers">
 
 
